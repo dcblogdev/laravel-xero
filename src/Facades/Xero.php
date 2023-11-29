bea6fe7ad33449ad7a337c89cc6547d5de6ee74f
Ben Lumley <ben@benlumley.co.uk>
2023-11-29 14:41:48 +0000

    Fix class inheritance, or mockery can't work
    Also add docblocks to make phpstan happy

--- src/Facades/Xero.php
+++ src/Facades/Xero.php
@@ -4,6 +4,14 @@ namespace Dcblogdev\Xero\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array get (string $endpoint, array $params = [])
+ * @method static array put (string $endpoint, array $params = [])
+ * @method static array post (string $endpoint, array $params = [])
+ * @method static array patch (string $endpoint, array $params = [])
+ * @method static array delete (string $endpoint, array $params = [])
+ * @mixin \Dcblogdev\Xero\Xero
+ */
 class Xero extends Facade
 {
     /**
