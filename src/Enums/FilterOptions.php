51a22d8d395739011368c43aa757ea0aed56b6b8
diaz <diazgunturfebrian26@gmail.com>
2024-10-17 14:47:12 +0800

    add statuses as a valid filter option

--- src/Enums/FilterOptions.php
+++ src/Enums/FilterOptions.php
@@ -11,6 +11,7 @@ enum FilterOptions: string
     case SEARCHTERM = 'searchTerm';
     case SUMMARYONLY = 'summaryOnly';
     case WHERE = 'where';
+    case STATUSES = 'Statuses';
 
     public static function isValid(string $value): bool
     {
